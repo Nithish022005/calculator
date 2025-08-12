12a70115dc0dc5ae66faf64c62ece70630d9f530
Nithish022005 <nithishwarannithish03@gmail.com>
2025-08-12 09:22:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+while(true){
+var a=parseInt(prompt("enter 1st number"));
+var b=parseInt(prompt("enter 2nd number"));
+var op=prompt("enter operater");
+var res;
+if(op==='+'){
+    res=a+b;
+    document.getElementById('para').innerHTML+="output is:"+res;
+    break; 
+}
+else if(op==='-'){
+    res=a-b;
+    document.getElementById('para').innerHTML+="output is:"+res;
+    break;
+}
+else if(op==='*'){
+    res=a*b;
+    document.getElementById('para').innerHTML+="output is:"+res;
+    break;
+}
+else if(op==='/'){
+    res=a/b;
+     document.getElementById('para').innerHTML+="output is:"+res;
+     break;
+}
+else{
+    res='Invalid Operator';
+    document.getElementById('para').innerHTML+="output:"+res;
+    break;
+}
+}
